0473c830e8f23be600a9f8548333196e8ebb872e
Harry Bridgen <105605342+BHarrry@users.noreply.github.com>
2025-08-01 14:38:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program {
+    static void Main(String[] args) {
+        someLinkedList<int> linkedlist = new someLinkedList<int>();
+        linkedlist.printList();
+        linkedlist.append(2);
+        linkedlist.printList();
+        linkedlist.append(3);
+        linkedlist.printList();
+        linkedlist.printListReverse();
+    }
+}
+
---
+++ someLinkedList.cs
@@ -0,0 +1,69 @@
+﻿public class someLinkedList<T> {
+
+    private LinkedListNode? head;
+    private LinkedListNode? tail;
+
+    public class LinkedListNode {
+        public T data { get; set; }
+        public LinkedListNode? next { get; set; }
+        public LinkedListNode? prev { get; set; }
+        public LinkedListNode(T data) {
+            this.data = data;
+        }
+    }
+
+    public void append(T data) {
+        LinkedListNode newNode = new LinkedListNode(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void prepend(T data) {
+        LinkedListNode newNode = new LinkedListNode(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            head.next = newNode;
+            newNode.prev = head;
+            head = newNode;
+        }
+    }
+
+    public void printList() {
+        if (head == null) {
+            Console.WriteLine("Head is null");
+            return;
+        }
+
+        LinkedListNode current = head;
+        while (current != null) {
+            Console.WriteLine(current.data);
+            current = current.next;
+
+        }
+    }
+
+    public void printListReverse() {
+        if (tail == null) {
+            Console.WriteLine("Tail is null");
+            return;
+        }
+
+        LinkedListNode current = tail;
+        while (current != null) {
+            Console.WriteLine(current.data);
+            current = current.prev;
+
+        }
+    }
+}
+
